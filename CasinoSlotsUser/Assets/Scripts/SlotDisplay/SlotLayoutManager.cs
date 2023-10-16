1b262bc29847b3b847d08ac584bd7894a63a6ada
Ryan <stander.ryan@gmail.com>
2023-10-16 23:15:26 +0200

    spinning (visual)

--- CasinoSlotsUser/Assets/Scripts/SlotDisplay/SlotLayoutManager.cs
+++ CasinoSlotsUser/Assets/Scripts/SlotDisplay/SlotLayoutManager.cs
@@ -7,6 +7,7 @@ namespace SlotDisplay
     /// </summary>
     public class SlotLayoutManager : MonoBehaviour
     {
+        [SerializeField] private GameObject[] slotRows;
         [SerializeField] private GameObject slotPrefab;
         [SerializeField, Range(1, 10)] private int reelCount = 3;
         [SerializeField, Range(1, 10)] private int rowCount = 3;
@@ -14,6 +15,8 @@ namespace SlotDisplay
         [SerializeField, Range(0.1f, 25f)] private float rowSpacing = 1f;
         public int ReelCount => reelCount;
         public int RowCount => rowCount;
+        public float RowSpacing => rowSpacing;
+        public GameObject[] SlotRows => slotRows;
 
         public GameObject[,] SlotBoard { get; private set; }
 
@@ -22,24 +25,35 @@ namespace SlotDisplay
         {
             if (Application.isPlaying)
                 return;
+
             if (slotPrefab == null)
                 return;
 
-            //destroy all children
-            foreach (Transform child in transform)
+            if (slotRows.Length != rowCount+1)
             {
-                UnityEditor.EditorApplication.delayCall += () => { DestroyImmediate(child.gameObject); };
+                Debug.LogError("Slot rows length needs to be equal to the row count + 1");
+                return;
             }
 
-            SlotBoard = new GameObject[reelCount, rowCount];
+            //destroy all children
+            foreach (var slotRow in slotRows)
+            {
+                foreach (Transform child in slotRow.transform)
+                {
+                    UnityEditor.EditorApplication.delayCall += () => { if(child!=null) DestroyImmediate(child.gameObject); };
+                }
+            }
+
+
+            SlotBoard = new GameObject[reelCount, rowCount+1];
 
             //create new children and name them to their index
             for (var i = 0; i < reelCount; i++)
             {
-                for (var j = 0; j < rowCount; j++)
+                for (var j = 0; j <= rowCount; j++)
                 {
-                    var slot = Instantiate(slotPrefab, transform);
-                    slot.transform.localPosition = new Vector3(i * reelSpacing, -j * rowSpacing, 0);
+                    var slot = Instantiate(slotPrefab, slotRows[j].transform);
+                    slot.transform.localPosition = new Vector3(i * reelSpacing, 0, 0);
                     slot.name = $"{i},{j}";
                     SlotBoard[i, j] = slot;
                 }
--- CasinoSlotsUser/Assets/Scripts/SlotFunctionality/SlotGameManager.cs
+++ CasinoSlotsUser/Assets/Scripts/SlotFunctionality/SlotGameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using SlotDisplay;
 using UnityEngine;
 
@@ -12,8 +13,13 @@ namespace SlotFunctionality
         [SerializeField] private GameObject[] slotSymbols;
         [SerializeField] private GameObject slots;
         [SerializeField] private Vector3 symbolOffset = new(0, 0, -1);
+        [SerializeField,Range(-2f,-0.01f)] private float startingSpinSpeed = 0.1f;
 
         private GameObject[,] symbols;
+        private List<GameObject> matchLines = new();
+
+        private float topRowYPos;
+        private float bottomRowYPos;
 
         private void OnValidate()
         {
@@ -28,28 +34,129 @@ namespace SlotFunctionality
         {
             symbols = new GameObject[slotLayoutManager.ReelCount, slotLayoutManager.RowCount];
             SpinTheWheel();
+
+            topRowYPos = slotLayoutManager.SlotRows[0].transform.localPosition.y;
+            bottomRowYPos = slotLayoutManager.SlotRows[^1].transform.localPosition.y-slotLayoutManager.RowSpacing;
+        }
+
+        private void Update()
+        {
+            //take the objs and move them down on the y axis
+            foreach (var obj in slotLayoutManager.SlotRows)
+            {
+                obj.transform.position += new Vector3(0, startingSpinSpeed, 0);
+
+                var rowTransform = obj.transform;
+
+                if (rowTransform.localPosition.y <= bottomRowYPos)
+                {
+                    rowTransform.localPosition =
+                        new Vector3(obj.transform.localPosition.x, topRowYPos, rowTransform.localPosition.z);
+                }
+            }
         }
 
         public void SpinTheWheel()
         {
+            //Clear lines from previous spin
+            foreach (var line in matchLines)
+            {
+                Destroy(line);
+            }
+
+            matchLines.Clear();
+
             for (var i = 0; i < slotLayoutManager.ReelCount; i++)
             {
                 for (var j = 0; j < slotLayoutManager.RowCount; j++)
                 {
-                    var gridPosition =  slotLayoutManager.SlotBoard[i, j];
-                    
+                    var gridPosition = slotLayoutManager.SlotBoard[i, j];
+
                     foreach (Transform child in gridPosition.transform)
                     {
                         Destroy(child.gameObject);
                     }
 
                     var pieceType = slotSymbols[Random.Range(0, slotSymbols.Length)];
-                    var thisPiece = Instantiate(pieceType, gridPosition.transform.position + symbolOffset, Quaternion.identity);
-                    thisPiece.name=pieceType.name;
+                    var thisPiece = Instantiate(pieceType, gridPosition.transform.position + symbolOffset,
+                        Quaternion.identity);
+                    thisPiece.name = pieceType.name;
                     thisPiece.transform.parent = gridPosition.transform;
                     symbols[i, j] = thisPiece;
                 }
             }
+
+            CheckForMatches();
+        }
+
+        private void DrawLine(Vector3 start, Vector3 end)
+        {
+            var myLine = new GameObject();
+            myLine.transform.position = start;
+            myLine.AddComponent<LineRenderer>();
+            var lineRenderer = myLine.GetComponent<LineRenderer>();
+            lineRenderer.startWidth = 0.1f;
+            lineRenderer.endWidth = 0.1f;
+            lineRenderer.SetPosition(0, start);
+            lineRenderer.SetPosition(1, end);
+            matchLines.Add(myLine);
+        }
+
+        private void CheckForMatches()
+        {
+            #region Vertical Matches
+
+            for (var i = 0; i < slotLayoutManager.ReelCount; i++)
+            {
+                for (var j = 0; j < slotLayoutManager.RowCount; j++)
+                {
+                    if (j != 0 && symbols[j, i].name != symbols[j - 1, i].name)
+                        break;
+                    if (j == slotLayoutManager.RowCount - 1)
+                    {
+                        DrawLine(symbols[0, i].transform.position + symbolOffset,
+                            symbols[slotLayoutManager.RowCount - 1, i].transform.position + symbolOffset);
+                    }
+                }
+            }
+
+            #endregion
+
+            #region Horizontal Matches
+
+            for (var i = 0; i < slotLayoutManager.RowCount; i++)
+            {
+                var matchLength = 1;
+                var matchBegin = symbols[i, 0];
+                GameObject matchEnd;
+                for (var j = 0; j < slotLayoutManager.ReelCount - 1; j++)
+                {
+                    if (symbols[i, j].name == symbols[i, j + 1].name)
+                    {
+                        matchLength++;
+                    }
+                    else
+                    {
+                        if (matchLength >= 3)
+                        {
+                            matchEnd = symbols[i, j];
+                            DrawLine(matchBegin.transform.position + symbolOffset,
+                                matchEnd.transform.position + symbolOffset);
+                        }
+
+                        matchBegin = symbols[i, j + 1];
+                        matchLength = 1;
+                    }
+                }
+
+                if (matchLength >= 3)
+                {
+                    matchEnd = symbols[i, slotLayoutManager.ReelCount - 1];
+                    DrawLine(matchBegin.transform.position + symbolOffset, matchEnd.transform.position + symbolOffset);
+                }
+            }
+
+            #endregion
         }
     }
 }
